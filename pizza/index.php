f5306537e5255e150777ba3cdebb36a3f1af9c43
Davidoff <davidoff@iMac-Davidoff.local>
2017-06-08 19:44:36 +0300

    Переделал индекс,добавил свич и шаблон success

--- pizza/index.php
+++ pizza/index.php
@@ -5,13 +5,33 @@
     define('APP_ROOT', __DIR__);
     define('DS', DIRECTORY_SEPARATOR);
     include APP_ROOT . DS . 'func.php';
+    define('ACTION_DEFAULT', 'form');
+    define('ACTION_CHEKCOMP', 'price');
+    define('ACTION_MAKEPIZZA', 'makepizza');
+
+    //Выполняет проверку на то что находиться в адресной строке, и записываем значение в переменную $action
+    if (!empty($_POST['Pizza'])) {
+        $action = ACTION_CHEKCOMP;
 
 
-if(!empty($_POST['Pizza'])) {
-        include APP_ROOT . DS . 'tpl' . DS . 'price.phtml';
-    } else {
-        include APP_ROOT . DS . 'tpl' . DS . 'form.phtml';
+    } elseif (!empty($_POST) && $_POST['Order'] ) {
+        $action = ACTION_MAKEPIZZA;
+    }else {
+        $action = ACTION_DEFAULT;
     }
 
-    var_dump($_POST);
+    switch($action) {
+     
+        case ACTION_CHEKCOMP:
+            include APP_ROOT . DS . 'tpl' . DS . 'price.phtml';
+            break;
+        case ACTION_MAKEPIZZA:
+            include APP_ROOT . DS . 'tpl' . DS . 'success.phtml';
+            break;
+        default:
+            include APP_ROOT . DS . 'tpl' . DS . 'form.phtml';
+            break;
+    }
+
+//    var_dump($_POST);
 ?>
