f24c36dbb312dbbd52915406c751150d0009dc79
Davidoff <davidoff@iMac-Davidoff.local>
2017-06-11 23:02:31 +0300

    Урок 4 задача с постами

--- lavrik/urok3.php
+++ lavrik/urok3.php
@@ -14,6 +14,12 @@
 //    }
 //    $x++;
 //}
+
+
+
+
+
+
 //$x = 0;
 //do {
 //    if($x == 0) {
@@ -26,58 +32,144 @@
 //    $x++;
 //} while ($x <= 10);
 //
+
+
+
+
+
+
 //for($i = 0;  $i<10; print $i, $i++);
 
 
 
-//$city = [
-//    'Kersonskaja' => ['Кхерсон', 'Каховка'],
-//    'Odeskaja' => ['Odessa', 'Illichevsk'],
-//    'Рязанская область' => ['Рязань', 'Павловск', 'Красноярск']
-//];
-//
-//foreach ($city as $obl => $town) {
-//    echo '<h1>' . $obl . '</h1>';
-//    echo '<ul>';
-//
-//
-//    foreach ($town as $item) {
-//        $goroda = '';
-//        if(substr($item, 0, 2) == 'К'){
-//            $goroda .= $item;
-//        }
-//
-//        echo '<li>'. $goroda .'</li>';
-//    }
-//    echo '</ul>';
-//}
+$city = [
+    'Kersonskaja' => ['Кхерсон', 'Каховка'],
+    'Odeskaja' => ['Одесса', 'Илличевск'],
+    'Рязанская область' => ['Рязань', 'Павловск', 'Красноярск']
+];
+
+function cityByName ($city, $chars = '') {
+    foreach ($city as $obl => $town) {
+        echo '<h1>' . $obl . '</h1>';
+        echo '<ul>';
+
+
+        foreach ($town as $key => $item) {
+            $goroda = '';
+
+            if(substr($item, 0, 2) == $chars) {
+
+                if ($key == count($town) - 1) {
+                    $goroda .= $item;
+                } else {
+
+                    $goroda .= $item . ',';
+                }
+
+            } elseif (empty($chars)) {
+                if($key == count($town) - 1) {
+                    $goroda .= $item;
+                } else {
+
+                    $goroda .= $item . ',';
+                }
+            }
+
+            echo '<li>'. $goroda .'</li>';
+        }
+        echo '</ul>';
+    }
+
+}
+
+cityByName ($city);
 
 //    echo $obl . ':' . '<br>' . '&nbsp;&nbsp;' . implode(', ', $town) . '<br>';
 
 
+//
+//function translit ($str) {
+//    $chars = [
+//        'а' => 'a',
+//        'б' => 'b',
+//        'в' => 'v',
+//        'г' => 'g',
+//        'д' => 'd',
+//        ' ' => '_'
+//    ];
+//    $newstr = '';
+//        for ($i = 0; $i < strlen($str); $i++) {
+//
+//            foreach ($chars as $key => $value) {
+//                if ($str[$i] == $key) {
+//                    $newstr .= $value;
+////                    if($str[$i] == ' '){
+////                        $newstr .= '_';
+////                    }
+//                }
+//            }
+//        }
+//    return $newstr;
+//}
+//
+//
+//
+//echo translit('дбв габв');
 
-function translit ($str) {
-    $chars = [
-        'а' => 'a',
-        'б' => 'b',
-        'в' => 'v',
-        'г' => 'g',
-        'д' => 'd',
-        ' ' => '_'
-    ];
-    $newstr = '';
+//function translit ($str) {
+//    $output = '';
+//    $chars = [
+//        'а' => 'a',
+//        'б' => 'b',
+//        'в' => 'v',
+//        'г' => 'g',
+//        'д' => 'd',
+//        ' ' => '_'
+//    ];
+//    for ($i = 0; $i < strlen($str); $i++) {
+//            $output .= $chars[$str[$i]];
+//
+//
+//        }
+//    return $output;
+//}
+//
+//function changeSpace ($str) {
+//    $space = explode(' ', $str);
+//    $slesh = implode('_', $space);
+//    return $slesh;
+//}
+
+function trans ($str) {
+
+    function translit($str) {
+        $output = '';
+        $chars = [
+            'а' => 'a',
+            'б' => 'b',
+            'в' => 'v',
+            'г' => 'g',
+            'д' => 'd',
+            ' ' => '_'
+        ];
         for ($i = 0; $i < strlen($str); $i++) {
+            $output .= $chars[$str[$i]];
 
-            foreach ($chars as $key => $value) {
-                if ($str[$i] == $key) {
-                    $newstr .= $value;
-//                    if($str[$i] == ' '){
-//                        $newstr .= '_';
-//                    }
-                }
-            }
+
         }
-    return $newstr;
+        return $output;
+
+    }
+
+    function changeSpace($str) {
+        $space = explode(' ', $str);
+        $slesh = implode('_', $space);
+        return $slesh;
+    }
+
+    return changeSpace(translit($str));
+
 }
 
-echo translit('дбв габв');
+
+echo trans('ббб');
