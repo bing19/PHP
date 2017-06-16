aca50e4eb2a6066ca01dcbc73d57a419bf3638a6
Davidoff <davidoff@iMac-Davidoff.local>
2017-06-16 22:09:59 +0300

    All delete

--- andrey/form.php
+++
@@ -1,5 +0,0 @@
-<form action="" method = "GET">
-    <input type="tel" name="telephone">
-    <input type="text" name="action" value="write" hidden>
-    <input type="submit" value="Записать">
-</form>
