e227baedb809d86f8344f4cd12e3debbcfb4226f
Davidoff <davidoff@iMac-Davidoff.local>
2017-06-22 23:25:51 +0300

    Добавил массив для проверки содержится ли допустимая операция в массиве

--- classes/Calc.php
+++ classes/Calc.php
@@ -5,23 +5,32 @@ class classes_Calc
 {
     public $x;
     public $y;
+    public $validOperation = ['+', '-', '*', '/'];
+    protected $operation;
     protected $res;
 
     public function calc ($x, $y, $operation) {
         $this->x = (int) $x;
         $this->y = (int) $y;
+        $this->operation = $operation;
 
-        switch ($operation) {
-            case '+':
-                return $this->res = $this->x + $this->y;
-                break;
-            case '-':
-                return $this->res = $this->x - $this->y;
-                break;
-            default:
-                return $this->res = null;
-                break;
+        if (in_array($this->operation, $this->validOperation)) {
+            switch ($operation) {
+                case '+':
+                    return $this->res = $this->x + $this->y;
+                    break;
+                case '-':
+                    return $this->res = $this->x - $this->y;
+                    break;
+                default:
+                    return $this->res = null;
+                    break;
+            }
+        } else {
+            return null;
         }
+
+
 
     }
 
